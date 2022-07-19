140b9953b344c3eabb68f2604ffcf16bbc1a67ad
Alikhan Amandyk <alikhan.amandyk@dnapayments.com>
2022-07-19 13:45:20 -0500

    first commit

---
+++ src/main/java/cs545waa/lab2/Lab2Application.java
@@ -0,0 +1,13 @@
+package cs545waa.lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2Application.class, args);
+	}
+
+}

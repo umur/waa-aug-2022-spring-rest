f9451e21f2b67923e5c5ab803d39147455fd757a
Shokhrukh Toshniyozov <shokhrukhtoshniyozov@Shokhrukhs-MacBook-Pro.local>
2022-07-19 20:27:37 -0500

    phases setup

---
+++ phase3/src/test/java/com/lab2/phase3/Phase3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab2.phase3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phase3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

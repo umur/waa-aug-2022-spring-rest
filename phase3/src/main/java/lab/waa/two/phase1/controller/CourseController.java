6fcbe66cb0d996ae014da39b1644b2d5f2ad2bbd
Amit <amit.ghosh@cefalo.com>
2022-07-20 21:11:47 -0500

    add client

--- phase3/src/main/java/lab/waa/two/phase1/controller/CourseController.java
+++ phase3/src/main/java/lab/waa/two/phase1/controller/CourseController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin
 @RequestMapping("/api/v1/courses")
 public class CourseController {
   private final CourseService courseService;
